829bd72392cce869df450856bd7a7b388feaa61c
Misbah58 <afsarmisbah@gmail.com>
2023-04-16 16:36:26 -0400

    the basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicStructure audi=new BasicStructure();
+        audi.color="black";
+        audi.power="A3";
+        audi.model="5000cc";
+    }
+}
